082afb506318fc96d5cae51b135e28f255184f25
TheAndruNique <theandrunique@gmail.com>
2024-03-28 18:38:34 +0300

    Fix errors

--- mwt/mwt.py
+++ mwt/mwt.py
@@ -10,7 +10,7 @@ def create_token(data: str, key: bytes, mwt_id: UUID, purpose: bytes) -> str:
     # encoding segments
     segments = [
         base64.urlsafe_b64encode(data.encode('utf-8')).rstrip(b'='),
-        base64.urlsafe_b64encode(purpose.value + b":" + mwt_id.bytes).rstrip(b'=')
+        base64.urlsafe_b64encode(purpose + b":" + mwt_id.bytes).rstrip(b'=')
     ]
     # creating and encoding hmac
     segments.append(
@@ -28,8 +28,8 @@ def create_token(data: str, key: bytes, mwt_id: UUID, purpose: bytes) -> str:
 def validate_token(token: str, key: bytes) -> tuple[str, UUID, bytes]:
     try:
         # segmentation
-        data_b64, purpose_b64, signature_b64 = token.split('.')
-        payload = base64.urlsafe_b64decode(align_b64(purpose_b64))
+        data_b64, payload_b64, signature_b64 = token.split('.')
+        payload = base64.urlsafe_b64decode(align_b64(payload_b64))
         purpose, mwt_id_bytes = payload.split(b":")
     except Exception:
         raise InvalidStructure("Invalid structure")
@@ -42,10 +42,10 @@ def validate_token(token: str, key: bytes) -> tuple[str, UUID, bytes]:
     if not validate_hmac(
         key=key,
         received_hmac=signature,
-        message=f"{data_b64}.{purpose_b64}".encode(),
+        message=f"{data_b64}.{payload_b64}".encode(),
     ):
         raise InvalidSignature("Invalid signature")
 
-    return data.decode("utf-8"), mwt_id 
+    return data.decode("utf-8"), mwt_id, purpose
 
 
