117636043e8dda527008d8c20efe1c2966e1e944
TheAndruNique <theandrunique@gmail.com>
2024-03-28 18:55:44 +0300

    Add more tests

--- tests/mwt_test.py
+++ tests/mwt_test.py
@@ -1,5 +1,11 @@
+import base64
+from uuid import uuid4
 import mwt
+from mwt.exceptions import InvalidToken
 from conftest import TEST_SECRET_KEY, mwt_id
+import pytest
+
+from mwt.utils import align_b64
 
 
 def test_create_token():
@@ -14,3 +20,25 @@ def test_create_token():
     assert data == decoded_data
     assert mwt_id == decoded_mwt_id
     assert purpose == decoded_purpose
+
+
+def test_invalid_token():
+    with pytest.raises(InvalidToken):
+        mwt.validate_token("invalid-token.efsdf.fsdlfhiosdf", TEST_SECRET_KEY)
+
+
+def test_create_token_and_change_content():
+    data = "data"
+    purpose = b"purpose"
+    token = mwt.create_token(data, TEST_SECRET_KEY, mwt_id, purpose)
+    
+    data, payload, signature = token.split(".")
+    
+    purpose, mwt_id_bytes = base64.urlsafe_b64decode(align_b64(payload)).split(b":")
+    new_mwt_id_bytes = uuid4().bytes
+    new_payload = base64.urlsafe_b64encode(purpose + b":" + new_mwt_id_bytes)
+    
+    interrupted_token = f"{data}.{new_payload}.{signature}"
+    
+    with pytest.raises(InvalidToken):
+        mwt.validate_token(interrupted_token, TEST_SECRET_KEY)
